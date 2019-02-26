1ab39e7f5f00849f6f88f1f63a2c0e60bdec587d
TakumiFujita <feelings.are.possibilities@gmail.com>
2019-02-26 21:19:22 +0900

    comment_ajax_function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,15 @@ class CommentsController < ApplicationController
     redirect_to "/tweets/#{@comment.tweet.id}"
   end
 
+  def destroy
+    tweet = Tweet.find(params[:tweet_id])
+    comment = Comment.find(params[:id])
+    if tweet.user_id
+      comment.destroy
+      redirect_to tweet_path(id: tweet.id), flash: {success: "コメントを削除しました"}
+    end
+  end
+
   private
   def commet_params
     params.permit(:text, :tweet_id)
